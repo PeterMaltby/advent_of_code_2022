1f7ae5079c202c4ecb9d57d305ac8aab19bff651
Peter Maltby <peter.p.maltby@gmail.com>
2022-12-08 21:11:47 +0000

    day 3 part 2 done.

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashSet;
+use itertools::izip;
 
 pub fn day3_1() {
 	let filename="day3/input.txt";
@@ -39,25 +39,35 @@ pub fn day3_2() {
 	let file = File::open(filename).unwrap();
 	let reader = BufReader::new(file);
 
-        let mut tmp :isize= 0;
+    let mut elf= 0;
+    let mut total= 0;
+
+    // 3 arrays of 52 booleans
+    let mut elfs_arr :[[bool; 52]; 3] = [[false;52];3];
 
 	for (_index, line) in reader.lines().enumerate() {
 		let line = line.unwrap();
-//                let v :Vec<u8>= line.bytes().map(|x| if x > 91 {x - 96 } else {x - 38}).collect();
+        let v :Vec<u8>= line.bytes().map(|x| if x > 91 {x - 96 } else {x - 38}).collect();
+        elfs_arr[elf] = [false; 52];
+        for item in v {
+            let index = item as usize;
+            elfs_arr[elf][index-1]=true;
+        }
 
-                let length = line.len()/2;
+        elf +=1;
 
-                let a = &line[0..length];
-                let b = &line[length..line.len()];
-                
-                for charecter in b.chars() {
-                    if a.contains(charecter) {
-                        let c = charecter as isize;
-                        let c2 = if c > 91 {c - 96 } else {c - 38};
-                        tmp += c2;
-                        break;
-                        }
+        if elf == 3 {
+            let mut val = 0;
+            for (a,b,c) in izip!(elfs_arr[0], elfs_arr[1],elfs_arr[2]) {
+                val += 1;
+                if a && b && c { 
+                    total += val;
+                    elf = 0;
+                    break;
                 }
-	}
-        println!("{}", tmp);
+            }
+        }
+    }
+
+    println!("{}", total);
 }
