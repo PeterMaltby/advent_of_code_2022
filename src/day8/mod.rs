0af8faf2684c31f30e14089b235b7840bab9eab6
Peter Maltby <Peter.Maltby1@sainsburys.co.uk>
2022-12-14 09:55:40 +0000

    day 8 part 2 done

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -19,9 +19,8 @@ pub fn day8_1() {
     let mut count = 0;
     for (x,row) in forest.iter().enumerate() {
         for (y,tree) in row.iter().enumerate() {
-            if x == 0 || y == 0 || x+1 == forest[0].len() || y+1 == forest.len() { count +=1; continue }
                 
-            //assumes visable and - if not
+            //assumes visable and - if not -1
             let mut angles_count = 4;
             //left
             for c in 0..y {
@@ -53,6 +52,55 @@ pub fn day8_1() {
 
 
 pub fn day8_2() {
-    println!("unsolved!");
+    let filename="day8/input.txt";
+
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut forest: Vec<Vec<u32>> = vec![Vec::new(); 0];
+    let mut scores: Vec<u32> = Vec::new();
+    
+    for line in reader.lines() {
+        let line = line.unwrap();
+        forest.push(line.chars().map(|x| x.to_digit(10).unwrap()).collect());
+    }
+
+    for (x,row) in forest.iter().enumerate() {
+        for (y,tree) in row.iter().enumerate() {
+                
+            let mut score = 0;
+            //left
+            for c in (0..y).rev() {
+                score +=1;
+                if tree <= &forest[x][c] { break; }
+            }
+            
+            let mut tmp = 0;
+            //right
+            for c in y+1..forest[y].len() {
+                tmp += 1;
+                if tree <= &forest[x][c] { break;  }
+            }
+            score = score * tmp;
+
+            let mut tmp = 0;
+            for c in (0..x).rev() {
+                tmp+=1;
+                if tree <= &forest[c][y] { break;}
+            }
+            score = score * tmp;
+
+            let mut tmp = 0;
+            for c in x+1..forest.len() {
+                tmp+=1;
+                if tree <= &forest[c][y] {break;}
+            }
+            
+            scores.push(score * tmp);
+
+        }
+    }
+
+    println!("{}", scores.iter().max().unwrap());
 }
 
