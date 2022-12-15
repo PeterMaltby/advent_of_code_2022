8ce39a28c9f0eba100e97ae1e5fbcc05ebd9526d
Peter Maltby <peter.p.maltby@gmail.com>
2022-12-15 23:29:51 +0000

    day 7 part 2 done, that was tough

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -14,18 +14,46 @@ pub struct Directory {
 impl Directory {
     fn print(&self) {
         
-        println!("{} directory contains {{",self.name);
+        println!("{} dir {} {{",self.name,self.du() );
         println!("{:?}",self.files.borrow());
         for sub_dir in self.sub_directories.borrow().values() {
-            sub_dir.as_ref().print();
+            sub_dir.print();
         }
         println!("}}-----------");
         
     }
+
+    fn du(&self) -> u32 {
+        let mut tmp_size = self.files.borrow().values().sum();
+
+        tmp_size +=  self.sub_directories.borrow().values().map(|x| x.du()).sum::<u32>();
+
+        tmp_size
+
+    }
+
+    fn prob(&self) -> u32 {
+        let du =  self.du();
+        let mut total = 0;
+        if du <= 100000 {total += du;}
+        total+= self.sub_directories.borrow().values().map(|x| x.prob()).sum::<u32>();
+
+        total
+    }
+
+
+    fn prob_2(&self, size: u32) {
+        let du = self.du();
+        if du > size {println!("{} : {}",self.name, du); }
+        for sub_dir in self.sub_directories.borrow().values() {
+            sub_dir.prob_2(size);
+        }
+    }
+
 }
 
 pub fn day7_1() {
-    let filename = "day7/testInput.txt";
+    let filename = "day7/input.txt";
 
     let file = FsFile::open(filename).unwrap();
     let reader = BufReader::new(file);
@@ -45,7 +73,6 @@ pub fn day7_1() {
 
         match (words[0], words[1]) {
             ("$", "ls") => {
-                println!("ls so ignore?");
             }
             ("$", "cd") => {
                 match words[2] {
@@ -74,14 +101,59 @@ pub fn day7_1() {
         }
     }
 
-    root.print();
 
-
-    println!("not done yet");
+    println!("{}, that took alot of work!",root.prob());
 }
 
 pub fn day7_2() {
     let filename = "day7/input.txt";
 
-    println!("not done yet!");
+    let file = FsFile::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let root = Rc::new(Directory {
+        parent: None,
+        name: String::from("/"),
+        sub_directories: RefCell::new(HashMap::new()),
+        files: RefCell::new(HashMap::new()),
+    });
+
+    let mut active_dir = Rc::clone(&root);
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let words = line.split(' ').collect::<Vec<&str>>();
+
+        match (words[0], words[1]) {
+            ("$", "ls") => {
+            }
+            ("$", "cd") => {
+                match words[2] {
+                    "/" => active_dir = Rc::clone(&root),
+                    ".." => active_dir = Rc::clone(&active_dir.parent.as_ref().unwrap()),
+                    goto_dir => {
+                        let new_active_dir = Rc::clone(&active_dir.sub_directories.borrow().get(goto_dir).unwrap());
+                        active_dir = new_active_dir;
+                    }
+                };
+            }
+            ("dir", new_dir_name) => {
+                let new_dir = Rc::new(Directory {
+                        parent: Some(Rc::clone(&active_dir)),
+                        name: new_dir_name.to_string(),
+                        sub_directories: RefCell::new(HashMap::new()),
+                        files: RefCell::new(HashMap::new()),
+                    });
+
+                active_dir.sub_directories.borrow_mut().insert(new_dir_name.to_string(), new_dir);
+
+            }
+            (filesize, filename) => {
+                active_dir.files.borrow_mut().insert(filename.to_string(), filesize.parse().unwrap());
+            }
+        }
+    }
+
+    let needed_space :u32 =  root.du()- (70_000_000 - 30_000_000) ;
+    root.prob_2(needed_space);
 }
